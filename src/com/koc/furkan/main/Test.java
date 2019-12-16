c1520b82ed18ba16783bc6e9475963acac4cedd4
Furkan KOC <furkankoc18@gmail.com>
2019-12-16 23:11:31 +0300

    log4j.properties file was updated and new property values was inserted.

--- src/com/koc/furkan/main/Test.java
+++ src/com/koc/furkan/main/Test.java
@@ -1,13 +1,19 @@
 package com.koc.furkan.main;
 
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 
 public class Test {
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger(Test.class);
-			logger.info("Furkan info");
-			logger.debug("Furkan debug");
+		
+		for(int i=0;i<1000;i++) {
+			logger.info("---------------------------");
+			logger.debug("Furkan debug "+i);
 			logger.error("Furkan error");
 			logger.fatal("Furkan fatal");
+
+		}
 	}
 }
